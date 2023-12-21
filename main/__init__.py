0cefbe4a5f9224d9f59e5321b08e1a7d797a1478
shivnagar232 <154045191+shivnagar232@users.noreply.github.com>
2023-12-21 20:24:26 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -17,10 +17,10 @@ logging.getLogger("telethon").setLevel(logging.WARNING)
 # variables
 API_ID = "26075120" #config("API_ID", default=None, cast=int)
 API_HASH = "1fda88a5d1de46058a4791c78bce198e" #config("API_HASH", default=None)
-BOT_TOKEN = "6508501033:AAHIFTa3Xbkx5NjcQPBnukt_KxdmSXfwHs4" #config("BOT_TOKEN", default=None)
-SESSION = "BQGTAMIAYFP0yfcy0sII4sAzyK5JjbGgFiMb6gVHy44_z4mACuzU6Gl8MGMl5h_89_Rt5kwcCskrNkRlQKA6NeS1R7FNzLy21QSKNxqTblzdwdyQGVNId15YnhH4ZZAGZiiJ69M0h6SRwac_b3m9nOHQnReXi0wdJ8tEG9CYc7kHXMLzQD8DrJh_RmJOnMwl0fSe_8FAxB4abVImyAHOOyZoqjrzeuPD-sSkUotycQ2IqXie6tyiNBypAmSfniAIkVGZXj500fJvb30X7_04C3IMYijpCzx22Mf_npl-xLavYBgOszFRT8SeQ6o6mBcR3svC-u3MMIvxE2JOmq59IskX9ZBDIgAAAAAiRSpcAA" #config("SESSION", default=None)
+BOT_TOKEN = "6707898403:AAHJWvJ1aHxJZgWzpZpuDSMs0VfebSrfLFs" #config("BOT_TOKEN", default=None)
+SESSION = "BQGTAMIAXQtr5-koY6BTlj8JejI0dGPGtUh-uwcqud5Lk-cY4Rlly9FXC4QxjXUpeorGqc2cih1N9pcOWduW0us1M4NmH-bu61tt5SFu_s5xbl_iO242yJsZPuIyrqnQLJM8wvGy1OuvEbO332F32awAlkLvik4FKpuqBIeyYEIQ_CYYIa7NpEWpBwy4w5fqe7ihbnlS_zXjhhvG05l7mEabLlqVzNykSQnzeFZ-OqQP8TY5qbqZRR3bto_G06KoK4-nDFQNrWt9fMaFsF1Gnpi1CRgqVBlsVGqXjLbdZQrb5790AQl9KQtHjIfv_g_hDylVGfOf_wax81uJdSF6bbiTEVQe_QAAAAGQ_sIBAA" #config("SESSION", default=None)
 FORCESUB = "dev_gagan" #config("FORCESUB", default=None)
-AUTH = "574958172" #config("AUTH", default=None))
+AUTH = "1874469918" #config("AUTH", default=None))
 SUDO_USERS = []
 if len(AUTH) != 0:
     SUDO_USERS = {int(AUTH.strip()) for AUTH in AUTH.split()}
